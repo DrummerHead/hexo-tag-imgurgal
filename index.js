0183f38728811af6571df2153caf914b16a53191
DrummerHead <drummerhead@gmail.com>
2016-03-19 12:56:25 -0300

    fix rule separation in inline style

--- index.js
+++ index.js
@@ -7,5 +7,5 @@
 
 hexo.extend.tag.register('imgurgal', function(args){
   var imgurId = args[0];
-  return "<iframe class='imgur-album' style='width: 100%, min-height: 90vh' src='//imgur.com/a/" + imgurId + "/embed'></iframe><a href='https://imgur.com/a/" + imgurId + "'>View gallery on Imgur</a>";
+  return "<iframe class='imgur-album' style='width: 100%; min-height: 90vh' src='//imgur.com/a/" + imgurId + "/embed'></iframe><a href='https://imgur.com/a/" + imgurId + "'>View gallery on Imgur</a>";
 })
